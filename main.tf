ad648ed0328ff02f13afb1368cd9ac82e5ce61a3
Chris Denton <chrisd@createtotally.com>
2023-04-13 14:22:14 +0100

    CR-4543 replace template_file with templatefile

--- main.tf
+++ main.tf
@@ -7,19 +7,16 @@ locals {
   }
 }
 
-data "template_file" "cloudformation_sns_stack" {
-  template = file("${path.module}/templates/email-sns-stack.json.tpl")
-
-  vars = {
-    display_name  = var.display_name
-    email_address = var.email_address
-    protocol      = var.protocol
-  }
-}
-
 resource "aws_cloudformation_stack" "sns-topic" {
   name          = var.stack_name
-  template_body = data.template_file.cloudformation_sns_stack.rendered
+  template_body = templatefile(
+    "${path.module}/templates/email-sns-stack.json.tpl",
+      {
+      display_name  = var.display_name
+      email_address = var.email_address
+      protocol      = var.protocol
+       }
+  )
 
   tags = merge(
     local.default_tags,
